9097baa51c1709ca96dbab9f21fd5f6573dfee8c
andrewtanjaya <andrewtanjaya21@gmail.com>
2021-07-13 15:22:11 +0700

    Maximize on open

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-
+  mainWindow.maximize()
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
